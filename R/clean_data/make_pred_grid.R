af1bcce7de830942437192b428d720a4ec4cbd2e
Max Lindmark <max.lindmark@slu.se>
2020-12-28 12:12:29 +0100

    new soruce for depth; add temperature to prediction grid

--- R/clean_data/make_pred_grid.R
+++ R/clean_data/make_pred_grid.R
@@ -6,14 +6,14 @@
 # 
 # A. Load libraries
 # 
-# B. Basic grid
+# B. Basic grid with depth
 #
-# C. Grid with oxygen
+# C. Grid with oxygen & temperature
 # 
 #%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 # A. LOAD LIBRARIES ================================================================
-#rm(list = ls())
+rm(list = ls())
 
 # Load libraries, install if needed
 library(tidyverse); theme_set(theme_classic())
@@ -39,6 +39,8 @@ library(png)
 # [1] forcats_0.5.0    stringr_1.4.0    dplyr_1.0.0      purrr_0.3.4      readr_1.3.1
 # tidyr_1.1.0      tibble_3.0.3    [8] ggplot2_3.3.2    tidyverse_1.3.0  glmmfields_0.1.4
 # Rcpp_1.0.5.1    
+
+world <- ne_countries(scale = "medium", returnclass = "sf")
 
 
 # B. BASIC GRID ====================================================================
@@ -74,53 +76,97 @@ pred_grid <- pred_grid %>%
 
 # Now we need to avoid predicting over land. We overlay the countries but it messes up 
 # the z and the legend... 
+# NOT NEEDED! Using the depth raster for that
 # https://stackoverflow.com/questions/23131359/r-keep-points-within-one-polygon
 # https://stackoverflow.com/questions/21567028/extracting-points-with-polygon-in-r
 # https://stackoverflow.com/questions/9333766/how-to-convert-a-spatial-dataframe-back-to-normal-dataframe
 
 # Read in the BITS shapefiles
-bits <- readOGR(dsn = "data/shapefiles")
-
-water_dat <- pred_grid %>% dplyr::select(lat, lon) %>% data.frame()
-
-coordinates(water_dat) <- ~ lon + lat
-
-proj4string(water_dat) <- proj4string(bits)
-
-water_dat <- water_dat[complete.cases(over(water_dat, bits)), ]
-
-water_dat <- as.data.frame(water_dat)
-
-head(water_dat)
+# bits <- readOGR(dsn = "data/shapefiles")
+# 
+# water_dat <- pred_grid %>% dplyr::select(lat, lon) %>% data.frame()
+# 
+# coordinates(water_dat) <- ~ lon + lat
+# 
+# proj4string(water_dat) <- proj4string(bits)
+# 
+# water_dat <- water_dat[complete.cases(over(water_dat, bits)), ]
+# 
+# water_dat <- as.data.frame(water_dat)
+# 
+# head(water_dat)
+# 
+# ggplot(water_dat, aes(lon, lat)) + geom_point()
 
 # Now we need to remove areas that haven't been sampled due to being too deep
+west <- raster("data/depth_geo_tif/D5_2018_rgb-1.tif")
+plot(west)
+
+east <- raster("data/depth_geo_tif/D6_2018_rgb-1.tif")
+plot(east)
+
+dep_rast <- raster::merge(west, east)
+
+plot(dep_rast)
+
 # To that by extracting depths of the pred grid
 # https://gis.stackexchange.com/questions/242941/extracting-values-from-raster-to-get-elevation-values-in-r
-baltic_sea <- getNOAA.bathy(lon1 = xmin, lon2 = xmax, lat1 = ymin, lat2 = ymax, resolution = 1)
+# baltic_sea <- getNOAA.bathy(lon1 = xmin, lon2 = xmax, lat1 = ymin, lat2 = ymax, resolution = 1)
+# 
+# plot(baltic_sea, image = TRUE)
+# scaleBathy(baltic_sea, deg = 2, x = "bottomleft", inset = 5)
+# 
+# latlong <- water_dat %>% dplyr::select(lon, lat)
+latlong <- pred_grid %>% dplyr::select(lon, lat)
+ 
+# data <- SpatialPoints(latlong)
+# 
+# plot(data, pch = 16, col = "red", add = TRUE)
+# 
+# sp <- get.depth(baltic_sea, latlong[, 1:2], locator = FALSE)
+latlong$depth <- extract(dep_rast, latlong[, 1:2])
+max(latlong$depth)
+min(latlong$depth)
 
-plot(baltic_sea, image = TRUE)
-scaleBathy(baltic_sea, deg = 2, x = "bottomleft", inset = 5)
+# Convert to depth (instead of elevation)
+ggplot(latlong, aes(depth)) + geom_histogram()
+latlong$depth <- latlong$depth - max(latlong$depth)
+ggplot(latlong, aes(depth)) + geom_histogram()
 
-latlong <- water_dat %>% dplyr::select(lon, lat)
+latlong %>% 
+  filter(depth < 0) %>% 
+  ggplot(., aes(lon, lat, color = depth)) + 
+  scale_color_viridis() +
+  geom_sf(data = world, inherit.aes = F, size = 0.2, fill = NA) +
+  geom_point(size = 1) + 
+  coord_sf(xlim = c(xmin, xmax), ylim = c(ymin, ymax)) +
+  NULL
 
-data <- SpatialPoints(latlong)
+latlong %>% 
+  filter(depth < 0) %>% 
+  ggplot(., aes(lon, lat, color = depth)) + 
+  scale_color_viridis() +
+  geom_sf(data = world, inherit.aes = F, size = 0.2, fill = NA, color = NA) +
+  geom_point(size = 1) + 
+  coord_sf(xlim = c(xmin, xmax), ylim = c(ymin, ymax)) +
+  NULL
 
-plot(data, pch = 16, col = "red", add = TRUE)
+latlong %>% 
+  filter(depth < 0) %>% 
+  ggplot(., aes(lon, lat, color = depth*-1)) + 
+  scale_color_viridis() +
+  geom_sf(data = world, inherit.aes = F, size = 0.2, fill = NA, color = NA) +
+  geom_point(size = 1) + 
+  coord_sf(xlim = c(xmin, xmax), ylim = c(ymin, ymax)) +
+  NULL
 
-sp <- get.depth(baltic_sea, latlong[, 1:2], locator = FALSE)
-
-df <- sp
-
-df$depth <- df$depth*-1
+df <- latlong %>% filter(depth < 0) %>% mutate(depth = depth*-1)
 
 # Now make a new grid. Can't use expand grid
 pred_grid <- data.frame(lon = rep(df$lon, length(unique(dat$year))),
                         lat = rep(df$lat, length(unique(dat$year))),
                         depth = rep(df$depth, length(unique(dat$year))),
                         year = rep(sort(unique(dat$year)), each = nrow(df)))
-
-# Plot nicer
-world <- ne_countries(scale = "medium", returnclass = "sf")
 
 p1 <- pred_grid %>%
   filter(year == "1999") %>% 
@@ -146,65 +192,26 @@ p2 <- pred_grid %>%
 
 p1 / p2
 
-# Plot depths
-pred_grid %>%
-  filter(year == "1999") %>% 
-  mutate(deep = ifelse(depth > 70, "Y", "N")) %>% 
-  ggplot(., aes(y = lat, x = lon, color = deep)) +
-  geom_sf(data = world, inherit.aes = F, size = 0.2, alpha = 0) +
-  coord_sf(xlim = c(xmin, xmax), ylim = c(ymin, ymax)) +
-  geom_point(size = 0.1) +
-  NULL
-
 # Now see how well it matches depth in data
 model_df <- readr::read_csv("https://raw.githubusercontent.com/maxlindmark/cod_condition/master/data/for_analysis/mdat_cond.csv")
 
-latlong2 <- model_df %>% dplyr::select(lon, lat)
+model_df$depth_bathy <- extract(dep_rast, model_df[, 4:3])
 
-data2 <- SpatialPoints(latlong2)
+model_df <- model_df %>% mutate(depth_bathy = (depth_bathy - max(depth_bathy)) * -1) 
 
-plot(baltic_sea, image = TRUE)
-scaleBathy(baltic_sea, deg = 2, x = "bottomleft", inset = 5)
-plot(data2, pch = 16, col = "red", add = TRUE)
-
-sp2 <- get.depth(baltic_sea, latlong2[, 1:2], locator = FALSE)
-
-df2 <- sp2
-
-df2$depth <- df2$depth*-1
-
-df2 <- df2 %>% rename("depth_raster" = "depth")
-
-df2$depth_bits <- model_df$depth
-
-ggplot(df2, aes(depth_raster, depth_bits)) +
-  geom_point() + 
-  geom_abline(color = "red")
-
-df2 %>% 
-  mutate(resid = depth_raster - depth_bits) %>% 
-  ggplot(., aes(resid)) +
-  geom_histogram()
-  
-df2 %>% 
-  mutate(resid = depth_raster - depth_bits) %>% 
-  ggplot(., aes(depth_bits, resid)) +
-  geom_point()
-
-# Plot on map
-df2 %>% 
-  mutate(resid = depth_raster - depth_bits) %>% 
-  ggplot(., aes(lon, lat, color = resid)) +
+model_df %>% 
+  drop_na(depth) %>% 
+  drop_na(depth_bathy) %>% 
+  ggplot(., aes(depth, depth_bathy)) +
   geom_point() +
-  scale_color_gradient2(midpoint = 0)
-
+  geom_abline(color = "red")
 
 # Save
 #write.csv(pred_grid, file = "data/for_analysis/pred_grid.csv", row.names = FALSE)
 
-# C. GRID WITH OXYGEN ==============================================================
-pred_grid2 <- pred_grid
 
+# C. GRID WITH OXYGEN & TEMPERATURE ================================================
+# ** Oxygen ========================================================================
 # Loop through each year and extract the oxygen levels
 # Downloaded from here: https://resources.marine.copernicus.eu/?option=com_csw&view=details&product_id=BALTICSEA_REANALYSIS_BIO_003_012
 # Extract raster points: https://gisday.wordpress.com/2014/03/24/extract-raster-values-from-points-using-r/comment-page-1/
@@ -275,6 +282,7 @@ months
 
 index_keep <- which(months > 9)
 
+# Quarter 4 by keeping months in index_keep
 oxy_q4 <- oxy_array[, , index_keep]
 
 months_keep <- months[index_keep]
@@ -315,7 +323,7 @@ names(dlist) <- unique(years_keep)
 # Now I need to make a loop where I extract the raster value for each year...
 
 # Filter years in the condition data frame to only have the years I have oxygen for
-d_sub_oxy <- pred_grid2 %>% filter(year %in% names(dlist)) %>% droplevels()
+d_sub_oxy <- pred_grid %>% filter(year %in% names(dlist)) %>% droplevels()
 
 # Create data holding object
 data_list <- list()
@@ -368,25 +376,27 @@ for(i in unique(d_sub_oxy$year_f)) {
 }
 
 # Now create a data frame from the list of all annual values
-pred_grid2 <- dplyr::bind_rows(data_list)
+pred_grid_oxy <- dplyr::bind_rows(data_list)
 
+lims <- pred_grid_oxy %>% drop_na(oxy) %>% summarise(min = min(oxy),
+                                                     max = max(oxy))
 # Plot and compare with rasters
-ggplot(pred_grid2, aes(lon, lat, color = oxy)) + 
+ggplot(pred_grid_oxy, aes(lon, lat, color = oxy)) + 
   facet_wrap(~year) +
   scale_colour_gradientn(colours = rev(terrain.colors(10)),
-                         limits = c(-200, 400)) +
+                         limits = c(lims$min, lims$max)) +
   geom_sf(data = world, inherit.aes = F, size = 0.2, alpha = 0) +
   coord_sf(xlim = c(xmin, xmax), ylim = c(ymin, ymax)) +
   geom_point(size = 0.1) +
   NULL
   
 # Animation
-pred_grid2$year <- as.integer(pred_grid2$year)
+pred_grid_oxy$year <- as.integer(pred_grid_oxy$year)
 
-p <- ggplot(pred_grid2, aes(lon, lat, fill = oxy)) + 
+p <- ggplot(pred_grid_oxy, aes(lon, lat, fill = oxy)) + 
   geom_raster() +
   scale_fill_gradientn(colours = rev(terrain.colors(10)),
-                         limits = c(min(drop_na(pred_grid2)$oxy), max(drop_na(pred_grid2)$oxy))) +
+                         limits = c(lims$min, lims$max)) +
   geom_sf(data = world, inherit.aes = F, size = 1, alpha = 0) +
   coord_sf(xlim = c(xmin, xmax), ylim = c(ymin, ymax))
   
@@ -402,7 +412,214 @@ animate(anim, height = 1200, width = 1200)
 anim_save(filename = "output/gif/oxy.gif")
 
 # Left join in the depth again
-pred_grid2 <- left_join(pred_grid2, pred_grid)
+pred_grid <- left_join(pred_grid_oxy, pred_grid)
 
 # Save
-#write.csv(pred_grid2, file = "data/for_analysis/pred_grid2.csv", row.names = FALSE)
+#write.csv(pred_grid, file = "data/for_analysis/pred_grid2.csv", row.names = FALSE)
+
+
+# ** Temperature ===================================================================
+# Open the netCDF file
+ncin <- nc_open("data/NEMO_Nordic_SCOBI/dataset-reanalysis-nemo-monthlymeans_1608127623694.nc")
+
+print(ncin)
+
+# Get longitude and latitude
+lon <- ncvar_get(ncin,"longitude")
+nlon <- dim(lon)
+head(lon)
+
+lat <- ncvar_get(ncin,"latitude")
+nlat <- dim(lat)
+head(lat)
+
+# Get time
+time <- ncvar_get(ncin,"time")
+time
+
+tunits <- ncatt_get(ncin,"time","units")
+nt <- dim(time)
+nt
+tunits
+
+# Get temperature
+dname <- "bottomT"
+
+temp_array <- ncvar_get(ncin,dname)
+dlname <- ncatt_get(ncin,dname,"long_name")
+dunits <- ncatt_get(ncin,dname,"units")
+fillvalue <- ncatt_get(ncin,dname,"_FillValue")
+dim(temp_array)
+
+# Get global attributes
+title <- ncatt_get(ncin,0,"title")
+institution <- ncatt_get(ncin,0,"institution")
+datasource <- ncatt_get(ncin,0,"source")
+references <- ncatt_get(ncin,0,"references")
+history <- ncatt_get(ncin,0,"history")
+Conventions <- ncatt_get(ncin,0,"Conventions")
+
+# Convert time: split the time units string into fields
+tustr <- strsplit(tunits$value, " ")
+tdstr <- strsplit(unlist(tustr)[3], "-")
+tmonth <- as.integer(unlist(tdstr)[2])
+tday <- as.integer(unlist(tdstr)[3])
+tyear <- as.integer(unlist(tdstr)[1])
+
+# Here I deviate from the guide a little bit. Save this info:
+dates <- chron(time, origin = c(tmonth, tday, tyear))
+
+# Crop the date variable
+months <- as.numeric(substr(dates, 2, 3))
+years <- as.numeric(substr(dates, 8, 9))
+years <- ifelse(years > 90, 1900 + years, 2000 + years)
+
+# Replace netCDF fill values with NA's
+temp_array[temp_array == fillvalue$value] <- NA
+
+# We only use Quarter 4 in this analysis, so now we want to loop through each time step,
+# and if it is a good month save it as a raster.
+# First get the index of months that correspond to Q4
+months
+
+index_keep <- which(months > 9)
+
+# Quarter 4 by keeping months in index_keep
+temp_q4 <- temp_array[, , index_keep]
+
+months_keep <- months[index_keep]
+
+years_keep <- years[index_keep]
+
+# Now we have an array with only Q4 data...
+# We need to now calculate the average within a year.
+# Get a sequence that takes every third value between 1: number of months (length)
+loop_seq <- seq(1, dim(temp_q4)[3], by = 3)
+
+# Create objects that will hold data
+dlist <- list()
+temp_10 <- c()
+temp_11 <- c()
+temp_12 <- c()
+temp_ave <- c()
+
+# Loop through the vector sequence with every third value, then take the average of
+# three consecutive months (i.e. q4)
+for(i in loop_seq) {
+  
+  temp_10 <- temp_q4[, , (i)]
+  temp_11 <- temp_q4[, , (i + 1)]
+  temp_12 <- temp_q4[, , (i + 2)]
+  
+  temp_ave <- (temp_10 + temp_11 + temp_12) / 3
+  
+  list_pos <- ((i/3) - (1/3)) + 1 # to get index 1:n(years)
+  
+  dlist[[list_pos]] <- temp_ave
+  
+}
+
+# Now name the lists with the year:
+names(dlist) <- unique(years_keep)
+
+# Now I need to make a loop where I extract the raster value for each year...
+# The condition data is called dat so far in this script
+
+# Filter years in the condition data frame to only have the years I have temperature for
+d_sub_temp <- pred_grid %>% filter(year %in% names(dlist)) %>% droplevels()
+
+# Create data holding object
+data_list <- list()
+
+# Create factor year for indexing the list in the loop
+d_sub_temp$year_f <- as.factor(d_sub_temp$year)
+
+# Loop through each year and extract raster values for the condition data points
+for(i in unique(d_sub_temp$year_f)) {
+  
+  # Subset a year
+  temp_slice <- dlist[[i]]
+  
+  # Create raster for that year (i)
+  r <- raster(t(temp_slice), xmn = min(lon), xmx = max(lon), ymn = min(lat), ymx = max(lat),
+              crs = CRS("+proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs+ towgs84=0,0,0"))
+  
+  # Flip...
+  r <- flip(r, direction = 'y')
+  
+  plot(r, main = i)
+  
+  # Filter the same year (i) in the condition data and select only coordinates
+  d_slice <- d_sub_temp %>% filter(year_f == i) %>% dplyr::select(lon, lat)
+  
+  # Make into a SpatialPoints object
+  data_sp <- SpatialPoints(d_slice)
+  
+  # Extract raster value (temperature)
+  rasValue <- raster::extract(r, data_sp)
+  
+  # Now we want to plot the results of the raster extractions by plotting the condition
+  # data points over a raster and saving it for each year.
+  # Make the SpatialPoints object into a raster again (for pl)
+  df <- as.data.frame(data_sp)
+  
+  # Add in the raster value in the df holding the coordinates for the condition data
+  d_slice$temp <- rasValue
+  
+  # Add in which year
+  d_slice$year <- i
+  
+  # Create a index for the data last where we store all years (because our loop index
+  # i is not continuous, we can't use it directly)
+  index <- as.numeric(d_slice$year)[1] - 1992
+  
+  # Add each years' data in the list
+  data_list[[index]] <- d_slice
+  
+}
+
+# Now create a data frame from the list of all annual values
+pred_grid_temp <- dplyr::bind_rows(data_list)
+
+lims <- pred_grid_temp %>% drop_na(temp) %>% summarise(min = min(temp),
+                                                       max = max(temp))
+
+# Plot and compare with rasters
+ggplot(pred_grid_temp, aes(lon, lat, color = temp)) + 
+  facet_wrap(~year) +
+  scale_colour_gradientn(colours = rev(terrain.colors(10)),
+                         limits = c(lims$min, lims$max)) +
+  geom_sf(data = world, inherit.aes = F, size = 0.2, alpha = 0) +
+  coord_sf(xlim = c(xmin, xmax), ylim = c(ymin, ymax)) +
+  geom_point(size = 0.1) +
+  NULL
+
+# Animation
+pred_grid_temp$year <- as.integer(pred_grid_temp$year)
+
+p <- ggplot(pred_grid_temp, aes(lon, lat, fill = temp)) + 
+  geom_raster() +
+  scale_fill_gradientn(colours = rev(terrain.colors(10)),
+                       limits = c(lims$min, lims$max)) +
+  geom_sf(data = world, inherit.aes = F, size = 1, alpha = 0) +
+  coord_sf(xlim = c(xmin, xmax), ylim = c(ymin, ymax))
+
+# Here comes the gganimate specific bits
+anim <- p +
+  labs(title = 'Year: {frame_time}') +
+  transition_time(as.integer(year)) +
+  ease_aes('linear') +
+  theme_classic(base_size = 24)
+
+animate(anim, height = 1200, width = 1200)
+
+anim_save(filename = "output/gif/temp.gif")
+
+# Left join in the depth again
+pred_grid <- left_join(pred_grid, pred_grid_temp)
+
+head(pred_grid)
+
+# Save
+write.csv(pred_grid, file = "data/for_analysis/pred_grid2.csv", row.names = FALSE)
+
