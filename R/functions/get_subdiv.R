da4d9e348c42c45ff9269c594eda9745b4dd55e4
Max Lindmark <max.lindmark@slu.se>
2021-08-27 14:24:21 +0200

    change name of sub div function

--- R/functions/get_subdiv.R
+++ R/functions/get_subdiv.R
@@ -1,5 +1,5 @@
-# Return sub_area based on coordinates
-get_sub_area <- function(dat, lon, lat){
+# Return sub_div based on coordinates
+get_sub_div <- function(dat, lon, lat){
 
   # Get ICES rect
   dat$ices_rect <- mapplots::ices.rect2(lon = lon, lat = lat)
