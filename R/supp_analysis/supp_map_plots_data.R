0434d3b4dc00c26fcd64ff77f230e94df5db5a59
Max Lindmark <max.lindmark@slu.se>
2022-03-07 21:58:09 +0100

    add ices rectangle figure to supplement

--- R/supp_analysis/supp_map_plots_data.R
+++ R/supp_analysis/supp_map_plots_data.R
@@ -5,11 +5,14 @@ rm(list = ls())
 library(tidyverse); theme_set(theme_light(base_size = 12))
 library(tidylog)
 library(sf)
+library(sp)
+library(raster)
 library(rnaturalearth)
 library(rnaturalearthdata)
 library(patchwork)
 library(RCurl)
 library(RColorBrewer)
+library(fishPifct)
 
 # For adding maps to plots
 # These are the ranges I'm thinking of using. Convert these to UTM!
@@ -33,25 +36,33 @@ utm_zone33 <- 32633
 swe_coast_proj <- sf::st_transform(swe_coast, crs = utm_zone33)
 
 # Read data
-pred_grid2 <- readr::read_csv("https://raw.githubusercontent.com/maxlindmark/cod_condition/master/data/for_analysis/pred_grid2.csv") %>% 
-  drop_na(oxy, depth, temp)
+pred_grid1 <- readr::read_csv("https://raw.githubusercontent.com/maxlindmark/cod_condition/master/data/for_analysis/pred_grid_(1_2).csv")
+pred_grid2 <- readr::read_csv("https://raw.githubusercontent.com/maxlindmark/cod_condition/master/data/for_analysis/pred_grid_(2_2).csv")
 
-cpue <- readr::read_csv("https://raw.githubusercontent.com/maxlindmark/cod_condition/master/data/for_analysis/mdat_cpue.csv") %>% 
+pred_grid <- bind_rows(pred_grid1, pred_grid2)
+
+cpue <- readr::read_csv("https://raw.githubusercontent.com/maxlindmark/cod_condition/master/data/for_analysis/mdat_cpue_q_1_4.csv") %>% 
   drop_na(oxy, depth, temp) %>% 
   mutate(Data = "CPUE") %>% 
   filter(lon > 12 & lat < 58) %>% 
   dplyr::select(Data, X, Y, year)
 
-cond <- readr::read_csv("https://raw.githubusercontent.com/maxlindmark/cod_condition/master/data/for_analysis/mdat_cond.csv") %>% 
-  drop_na(abun_spr, abun_her) %>% 
+cond1 <- readr::read_csv("https://raw.githubusercontent.com/maxlindmark/cod_condition/master/data/for_analysis/mdat_cond_(1_2).csv") %>% 
   mutate(Data = "Condition") %>% 
   filter(lon > 12 & lat < 58) %>% 
   dplyr::select(Data, X, Y, year)
 
+cond2 <- readr::read_csv("https://raw.githubusercontent.com/maxlindmark/cod_condition/master/data/for_analysis/mdat_cond_(2_2).csv") %>% 
+    mutate(Data = "Condition") %>% 
+    filter(lon > 12 & lat < 58) %>% 
+    dplyr::select(Data, X, Y, year)
+  
+cond <- bind_rows(cond1, cond2)
+
 hauls <- bind_rows(cpue, cond)
 
 p1 <- ggplot(swe_coast_proj) +
-  geom_raster(data = pred_grid2, aes(x = X*1000, y = Y*1000, fill = factor(SubDiv)), alpha = 0.8) +
+  geom_raster(data = pred_grid2, aes(x = X*1000, y = Y*1000, fill = factor(sub_div)), alpha = 0.8) +
   geom_sf() +
   scale_fill_brewer(palette = "Dark2") + 
   labs(x = "Longitude", y = "Latitude", fill = "SubDiv")
@@ -69,19 +80,53 @@ p2 <- ggplot(swe_coast_proj) +
   annotate("text", label = "Gotland", x = xmin + 0.7*xrange, y = ymin + 0.9*yrange, angle = 70, color = "tomato", size = 5) +
   labs(x = "Longitude", y = "Latitude")
 
-p1 / p2
+
+# Plot ICES rectangles
+# https://heima.hafro.is/~einarhj/spatialr/pre_ggplot2.html
+scale_longitude_ices <- function(min = -44, max = 68.5, step = 1, ...) {
+  breaks <- seq(min + 0.5, max - 0.5, step)
+  labels <- geo::d2ir(60, breaks) %>% str_sub(3)
+  return(scale_x_continuous(name = NULL,
+                            breaks = breaks,
+                            labels = labels,
+                            ...))
+}
+
+scale_latitude_ices <- function(min = 36, max = 84.5, step = 0.5, ...) {
+  breaks <- seq(min + 0.25, max - 0.25, step)
+  #labels <- geo::d2ir(breaks, 0) %>% str_sub(1, 2)
+  return(scale_y_continuous(name = NULL,
+                            breaks = breaks,
+                            #labels = labels,
+                            ...))
+}
+
+ir <- 
+  sf::read_sf("ftp://ftp.hafro.is/pub/data/shapes/ices_rectangles.gpkg")
+
+ir2 <- ir %>% filter(between(west, 12, 22),
+         between(south, 54, 58))
+
+p3 <- ggplot() +
+  geom_point(data = pred_grid2, aes(x = lon, y = lat), color = "skyblue") +
+  geom_text(data = ir2,
+            aes(x = west + 0.5, y = south + 0.25,
+                label = icesname),
+            angle = 45, 
+            colour = "red",
+            size = 3) +
+  scale_longitude_ices() +
+  scale_latitude_ices() +
+  theme(panel.grid.major = element_blank(),
+        panel.grid.minor = element_line(size = 1),
+        axis.ticks = element_blank(),
+        panel.background = element_blank()
+        , panel.ontop = TRUE
+        ) +
+  coord_quickmap() + 
+  NULL
+
+
+p1 / p3
 
 ggsave("figures/supp/map_subdiv_areas.png", width = 6.5, height = 6.5, dpi = 600)
-
-# ggplot(swe_coast_proj) +
-#   geom_point(data = hauls, aes(x = X*1000, y = Y*1000, color = Data), size = 0.2) +
-#   geom_sf(size = 0.3) +
-#   facet_wrap(~year, ncol = 5) +
-#   scale_fill_brewer(palette = "Dark2") + 
-#   labs(x = "Longitude", y = "Latitude") + 
-#   theme(axis.text.x = element_text(angle = 90),
-#         axis.text = element_text(size = 6),
-#         strip.text = element_text(size = 8, colour = 'black', margin = margin()),
-#         strip.background = element_rect(fill = "grey90"))
-# 
-# ggsave("figures/supp/data_position_year.png", width = 6.5, height = 6.5, dpi = 600)
